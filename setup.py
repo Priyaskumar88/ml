bf5f90bb301457a0c16b7ad2b9d3cb6f14960fe8
Priyaskumar88 <priyaskumar88@gmail.com>
2023-04-07 12:28:03 +0530

    git commit

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:  
+def get_requirements(file_path:str)->List[str]:
     '''
-    this function will return list of requirements
+    this function will return the list of requirements
     '''
     requirements=[]
-    with open(file_path)as file_obj:
+    with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n"," ")for req in requirements]
-        
+        requirements=[req.replace("\n","") for req in requirements]
+
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     
     return requirements
+
 setup(
 name='mlproject',
 version='0.0.1',
@@ -22,6 +23,5 @@ author='Priya',
 author_email='priyaskumar88@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
-
 
 )
